ad368630d876aff194157adadde4037ca588ce79
Prosto-Fil <prostofiliarus@gmail.com>
2020-06-06 20:25:06 +0300

    init commit

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int
+main(int argc, char* argv)
+{
+  string name; // uses string variable
+  cout << "Input user_name" : cin >> name;// reads your name
+  cout << "Hello world from " << name << endl; // prints greetings
+  return 0;
+}
